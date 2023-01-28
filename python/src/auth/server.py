224279a87db2e7071bacd2b333489dd34c127341
harshad <harshadchavan141@gmail.com>
2023-01-28 23:44:07 +0530

    createed func to create jwt and validate it,dockerfiel added unique contraint to email field

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -24,4 +24,67 @@ def login():
     if not auth:
         return "Missing credentials", 401
     else:
-        pass
+        cur = mysql.connection.cursor()
+        res = cur.execute("SELECT email FROM user WHERE email=%s", (auth.username,))
+
+        if res > 0:
+            # user exists
+            user_row = cur.fetchone()
+            email = user_row[0]
+            password = user_row[1]
+
+            if auth.username != email or auth.password != password:
+                return "invalid credentials", 401
+            else:
+                return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
+        else:
+            return "Invalid credentials", 401
+
+
+def createJWT(username, secret, authz):
+    """
+    this method creates the jwt tokoen that will be sent ot the requesting user after validation of username & password.
+    this jwt token will be used by the user for subsequent requests.
+    jwt token comprises header, payload
+
+    :param username:
+    :param secret:
+    :param authz:
+    :return:
+    """
+    jwt.encode(
+        {
+            "username": username,
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(days=1),
+            "iat": datetime.datetime.utcnow(),
+            "admin": authz
+        },
+        secret,
+        algorithm="HS256"
+    )
+
+
+@server.route("/validate")
+def validate():
+    encoded_jwt = request.headers["Authorization"]
+
+    if not encoded_jwt:
+        return "missing credentials", 401
+
+    encoded_jwt = encoded_jwt.split(" ")[1]
+
+    try:
+        decoded = jwt.decode(encoded_jwt, os.environ.get("JWT_SECRET"), algorithms="HS256")
+    except:
+        return "Not authorized"
+
+    return decoded
+
+
+@server.route("/test")
+def test():
+    return "hello world"
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port="5000")
